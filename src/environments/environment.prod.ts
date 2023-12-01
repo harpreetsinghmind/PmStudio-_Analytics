c844089321a556eaa0e4f3602af1442c8bff7bda
Kunal Malhotra <kunal@mindweapons.in>
2023-12-01 19:30:25 +0530

    All Push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  siteUrl: 'https://demo.pm-studio.com/api/',
-  apiUrl: 'https://demo.pm-studio.com/api/api/',
+  siteUrl: 'https://app.pm-studio.com/api/',
+  apiUrl: 'https://app.pm-studio.com/api/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  siteUrl: 'https://demo.pm-studio.com/api/',
+  siteUrl: 'https://app.pm-studio.com/api/',
   domainUrl: 'https://demo.pm-studio.com/',
-  apiUrl: 'https://demo.pm-studio.com/api/api/',
+  apiUrl: 'https://app.pm-studio.com/api/api/',
   AnalyticKey: '9877769078@kunalMalhotra',
 };
 
